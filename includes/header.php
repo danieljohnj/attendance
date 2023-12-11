045f43d12ecd52b4f4ecd0d0309c4f9fc97a5aa7
Daniel-John Jackson <47371401+danieljohnj@users.noreply.github.com>
2023-12-11 14:37:42 -0500

    login and logout update

--- includes/header.php
+++ includes/header.php
@@ -14,14 +14,15 @@ include_once 'includes/session.php'?>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="css/site.css" />
-    <title>Attendance - <?php echo $title?></title>
+
+    <title>Attendance - <?php echo $title ?></title>
   </head>
   <body>
     <div class="container">
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="index.php">IT Conference</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavaltmarkup" aria-controls="navbarNavaltmarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltmarkupNav">
@@ -30,7 +31,15 @@ include_once 'includes/session.php'?>
         <a class="nav-link nav-link" href="viewrecords.php">View Attendees</a>
     </div>
     <div class="navbar-nav ml-auto">
-        <a class="nav-item nav-link " href="login.php">Login <span class="sr-only">(current)</span></a>
+        <?php
+          if(!isset($_SESSION['userid'])){         
+        ?>
+          <a class="nav-item nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
+          <?php }else{ ?>
+              <span>Hello <?php echo $_SESSION['username'] ?>! </span>
+              <a class="nav-item nav-link" href="logout.php">Logout<span class="sr-only">(current)</span></a>
+
+            <?php }?>
     </div>
   </div>
 </nav>
